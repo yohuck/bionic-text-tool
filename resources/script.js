b06e4f93b7694c8fc9afb6d023cd5162cefb8bd2
Eric Lake <ericallenlake@gmail.com>
2022-05-20 16:59:37 -0600

    removes lorem

--- resources/script.js
+++ resources/script.js
@@ -5,7 +5,7 @@ const pasteIn = document.getElementById('pasteIn');
 
 
 let textToConvert = pasteIn.value
-let convertedText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sed porta ante. Phasellus placerat facilisis enim non bibendum. Cras eu nulla risus. Aliquam tortor quam, fringilla id magna vel, ullamcorper malesuada leo. Etiam vel aliquet mauris. Phasellus nec est sed mauris cursus posuere. Ut mattis suscipit metus sed mattis. Morbi."
+let convertedText = ""
 dataList.innerHTML = convertedText;
 fetchButton.addEventListener('click', function(e){
     e.preventDefault()
@@ -36,8 +36,8 @@ const options = {
 fetch('https://bionic-reading1.p.rapidapi.com/convert', options)
 .then (response => response.text().then(function(text) {
 
-    var x = text.toString();
-    convertedText = x;
+    var sendIt = text.toString();
+    convertedText = sendIt;
     dataList.innerHTML = convertedText;
 }))
 .catch(err => console.error(err));
